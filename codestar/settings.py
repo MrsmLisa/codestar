046f05c55f6c82018a2bf799391a8dce011d9d75
mrsmlisa <lisa.q.scott@gmail.com>
2026-05-13 09:42:33 +0200

    debugging

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-#+-2+66a!=^$rf12c19vuiq@kmc90adnod)n7(4n%aa-k^gd6#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 '127.0.0.1',]
 
 
 # Application definition
