f7a20409626df58480191f50b82bdb926aa9b9f8
mrsmlisa <lisa.q.scott@gmail.com>
2026-05-18 13:31:27 +0200

    Made some changes for whitenoise and added to the search feild in blog admin

--- codestar/settings.py
+++ codestar/settings.py
@@ -138,10 +138,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
