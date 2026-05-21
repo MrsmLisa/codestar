23dfea4c0680f95d28dfd94f499024e75bf64579
mrsmlisa <lisa.q.scott@gmail.com>
2026-05-21 14:36:36 +0200

    debugging commentsection

--- blog/views.py
+++ blog/views.py
@@ -28,20 +28,19 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
-
+    comment_form = CommentForm()
+    
     if request.method == "POST":
         comment_form = CommentForm(data=request.POST)
-    if comment_form.is_valid():
-        comment = comment_form.save(commit=False)
-        comment.author = request.user
-        comment.post = post
-        comment.save()
-        messages.add_message(
-            request, messages.SUCCESS,
-            'comment submitted and awating approval'
-        )
-
-    comment_form = CommentForm()
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'comment submitted and awating approval'
+            )
 
     return render(
         request,
@@ -49,6 +48,6 @@ def post_detail(request, slug):
         {"post": post,
          "comments": comments,
         "comment_count": comment_count,
-        "comment-form": comment_form
+        "comment_form": comment_form
         },
     )
